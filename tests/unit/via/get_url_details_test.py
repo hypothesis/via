8b975a1b114b8d96e305b5f95a99b35507ffbd58
Jon Betts <jon.betts@hypothes.is>
2020-04-07 17:50:40 +0100

    Add Google API mime-type checking for Google Drive URLs

--- tests/unit/via/get_url_details_test.py
+++ tests/unit/via/get_url_details_test.py
@@ -1,6 +1,7 @@
 from io import BytesIO
 
 import pytest
+from h_matchers import Any
 from requests import Response
 from requests.exceptions import (
     MissingSchema,
@@ -14,16 +15,13 @@ from via.get_url_details import get_url_details
 
 
 class TestGetURLDetails:
+    GOOGLE_API_KEY = "google_api_key"
+
     @pytest.mark.parametrize(
         "content_type,status_code", (("text/html", 501), ("application/pdf", 200))
     )
     def test_it_calls_get_for_normal_urls(self, requests, content_type, status_code):
-        response = Response()
-        response.raw = BytesIO(b"")
-        response.headers = {"Content-Type": content_type}
-        response.status_code = status_code
-        requests.get.return_value = response
-
+        requests.get.return_value = self._make_response(content_type, status_code)
         url = "http://example.com"
 
         result = get_url_details(url)
@@ -31,14 +29,40 @@ class TestGetURLDetails:
         assert result == (content_type, status_code)
         requests.get.assert_called_once_with(url, allow_redirects=True, stream=True)
 
-    def test_it_assumes_pdf_with_a_google_drive_url(self, requests):
-        result = get_url_details(
-            "https://drive.google.com/uc?id=--FILEID--&export=download"
-        )
+    def test_it_assumes_google_documents_are_pdfs_without_an_api_key(
+        self, requests, google_drive_url
+    ):
+        result = get_url_details(google_drive_url)
 
         assert result == ("application/pdf", 200)
-
         requests.get.assert_not_called()
+
+    @pytest.mark.usefixtures("with_api_key")
+    def test_it_calls_google_drive_api_with_api_key(self, requests, google_drive_url):
+        requests.get.return_value = self._make_response(
+            content_type="application/json", body='{"mimeType": "application/pdf"}'
+        )
+
+        get_url_details(google_drive_url)
+
+        requests.get.assert_called_once_with(
+            Any.url.matching(
+                "https://www.googleapis.com/drive/v3/files/--FILEID--"
+            ).with_query({"key": self.GOOGLE_API_KEY})
+        )
+
+    @pytest.mark.usefixtures("with_api_key")
+    def test_it_handles_bad_google_json_responses(self, requests, google_drive_url):
+        requests.get.return_value = self._make_response(
+            content_type="application/json", body="{"
+        )
+
+        with pytest.raises(UpstreamServiceError):
+            get_url_details(google_drive_url)
+
+    @pytest.fixture
+    def google_drive_url(self):
+        return "https://drive.google.com/uc?id=--FILEID--&export=download"
 
     @pytest.mark.parametrize("bad_url", ("no-schema", "glub://example.com", "http://"))
     def test_it_raises_BadURL_for_invalid_urls(self, bad_url):
@@ -61,6 +85,21 @@ class TestGetURLDetails:
 
         with pytest.raises(expected_exception):
             get_url_details("http://example.com")
+
+    @staticmethod
+    def _make_response(content_type="application/pdf", status_code=200, body=None):
+        response = Response()
+
+        response.raw = BytesIO(body.encode("utf-8") if body else b"")
+        response.headers = {"Content-Type": content_type}
+        response.status_code = status_code
+
+        return response
+
+    @pytest.fixture
+    def with_api_key(self, patch):
+        api_key = patch("via.get_url_details.GOOGLE_DRIVE_API_KEY")
+        api_key.__str__.return_value = self.GOOGLE_API_KEY
 
     @pytest.fixture
     def requests(self, patch):
--- via/get_url_details.py
+++ via/get_url_details.py
@@ -1,6 +1,8 @@
 """Retrieve details about a resource at a URL."""
+import os
 import re
 from functools import wraps
+from json import JSONDecodeError
 
 import requests
 from requests import RequestException
@@ -16,6 +18,8 @@ from via.exceptions import (
 GOOGLE_DRIVE_REGEX = re.compile(
     r"^https://drive.google.com/uc\?id=(.*)&export=download$", re.IGNORECASE
 )
+GOOGLE_DRIVE_API_KEY = os.environ.get("GOOGLE_DRIVE_API_KEY")
+GOOGLE_DRIVE_URL = "https://www.googleapis.com/drive/v3/files/"
 
 
 def _handle_errors(inner):
@@ -28,6 +32,9 @@ def _handle_errors(inner):
 
         except REQUESTS_BAD_URL as err:
             raise BadURL(err.args[0]) from None
+
+        except JSONDecodeError as err:
+            raise UpstreamServiceError(err.args[0]) from None
 
         except REQUESTS_UPSTREAM_SERVICE as err:
             raise UpstreamServiceError(err.args[0]) from None
@@ -50,8 +57,17 @@ def get_url_details(url):
     :raise UnhandledException: For all other request based errors
     """
 
-    if GOOGLE_DRIVE_REGEX.match(url):
-        return "application/pdf", 200
+    google_drive = GOOGLE_DRIVE_REGEX.match(url)
+    if google_drive:
+        if not GOOGLE_DRIVE_API_KEY:
+            return "application/pdf", 200
+
+        rsp = requests.get(
+            f"{GOOGLE_DRIVE_URL}{google_drive.group(1)}?key={GOOGLE_DRIVE_API_KEY}"
+        )
+        data = rsp.json()
+
+        return data["mimeType"], rsp.status_code
 
     with requests.get(url, stream=True, allow_redirects=True) as rsp:
         return rsp.headers.get("Content-Type"), rsp.status_code
