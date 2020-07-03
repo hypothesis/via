7b0e02235ea22fd1ee97b63cc33ebac447b04d93
Jon Betts <jon.betts@hypothes.is>
2020-07-03 17:55:43 +0100

    Adding a view to help debugging headers
    This is so we can capture example headers to know where to insert
    `Referer`.

--- via/views/__init__.py
+++ via/views/__init__.py
@@ -7,6 +7,7 @@ def add_routes(config):
     config.add_route("get_status", "/_status")
     config.add_route("view_pdf", "/pdf", factory=URLResource)
     config.add_route("route_by_content", "/route", factory=URLResource)
+    config.add_route("debug_headers", "/debug/headers")
 
 
 def includeme(config):
