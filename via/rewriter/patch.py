344868b8e9e9c2a7fe2fe4b5cfd86f4420ed5d19
Jon Betts <jon.betts@hypothes.is>
2020-08-10 17:02:21 +0100

    Reworking to get it going from tox

--- via/rewriter/patch.py
+++ via/rewriter/patch.py
@@ -1,38 +1,28 @@
 """Tools to apply the hooks to a running `pywb` app."""
-from pywb.apps.frontendapp import FrontEndApp
 from pywb.apps.rewriterapp import RewriterApp
 from pywb.rewrite.url_rewriter import UrlRewriter
 
 
 def apply_post_app_hooks(rewriter_app, hooks):
     # Add most hook points
-    PatchedRewriterApp.patch(rewriter_app, hooks)
+    _PatchedRewriterApp.patch(rewriter_app, hooks)
 
 
 def apply_pre_app_hooks(hooks):
     """Apply hooks before the app has been instantiated."""
 
     # Patch out URLs to ignore
-    patch_url_rewriter(hooks)
+    _patch_url_rewriter(hooks)
 
 
-def patch_url_rewriter(hooks):
+def _patch_url_rewriter(hooks):
     # Modify the list of prefixes that prevent a URL from being rewritten
     prefixes = list(UrlRewriter.NO_REWRITE_URI_PREFIX)
     prefixes.extend(hooks.ignore_prefixes)
     UrlRewriter.NO_REWRITE_URI_PREFIX = tuple(prefixes)
 
 
-class PatchedFrontEndApp(FrontEndApp):
-    def handle_request(self, environ, start_response):
-        # Ensure we can map in the script name from NGINX
-        if "HTTP_SCRIPT_NAME" in environ:
-            environ["SCRIPT_NAME"] = environ["HTTP_SCRIPT_NAME"]
-
-        return super().handle_request(environ, start_response)
-
-
-class PatchedRewriterApp(RewriterApp):
+class _PatchedRewriterApp(RewriterApp):
     hooks = None
 
     @classmethod
