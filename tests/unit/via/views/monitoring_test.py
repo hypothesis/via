45501d4aa61a2165d16dc42b617de7cf772e5721
Marcos Prieto <marcospri@gmail.com>
2021-02-18 10:05:00 +0100

    Return json on /_status endpoint to align with other services

--- tests/unit/via/views/monitoring_test.py
+++ tests/unit/via/views/monitoring_test.py
@@ -3,9 +3,8 @@ from via.views.monitoring import get_status
 
 class TestStatusRoute:
     def test_status_returns_200_response(self, make_request):
-        request = make_request("/status")
+        request = make_request("/_status")
 
         result = get_status(request)
 
-        assert result.status == "200 OK"
-        assert result.status_int == 200
+        assert result == {"status": "okay"}
