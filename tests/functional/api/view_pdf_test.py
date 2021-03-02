c7f7c27e9229f86ca9109d6bc527e3ec259cbf2b
Marcos Prieto <marcospri@gmail.com>
2021-03-02 11:49:50 +0100

    Remove checkmate_enabled flag

--- tests/functional/api/view_pdf_test.py
+++ tests/functional/api/view_pdf_test.py
@@ -1,7 +1,10 @@
+import pytest
+
 from tests.conftest import assert_cache_control
 
 
 class TestViewPDFAPI:
+    @pytest.mark.usefixtures("checkmate_pass")
     def test_caching_is_disabled(self, test_app):
         response = test_app.get("/pdf?url=http://example.com/foo.pdf")
 
--- tests/unit/via/views/route_by_content_test.py
+++ tests/unit/via/views/route_by_content_test.py
@@ -47,22 +47,6 @@ class TestRouteByContent:
 
         get_url_details.assert_called_once_with(url, Any.instance_of(EnvironHeaders))
 
-    def test_blocked_for_passed_to_viahtml(
-        self, call_route_by_content, via_client_service, get_url_details
-    ):
-        get_url_details.return_value = ("html", 200)
-        url = "http://example.com/path%2C?a=b"
-
-        call_route_by_content(
-            url,
-            params={"via.blocked_for": "lms"},
-            settings={"checkmate_enabled": False},
-        )
-
-        via_client_service.url_for.assert_called_once_with(
-            url, content_type="html", options={"via.blocked_for": "lms"}
-        )
-
     @pytest.mark.parametrize(
         "content_type,max_age", [("application/pdf", 300), ("text/html", 60)]
     )
@@ -103,10 +87,6 @@ class TestRouteByContent:
             target_url="http://example.com", params=None, settings=None
         ):
             request = make_request(params=dict(params or {}, url=target_url))
-            if settings:
-
-                for key, value in settings.items():
-                    request.registry.settings[key] = value
             context = URLResource(request)
 
             return route_by_content(context, request)
