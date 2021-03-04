dd595b2a96fb38f2aea98881b26ba870cd315719
Sean Hammond <seanh@users.noreply.github.com>
2021-03-04 15:42:44 +0000

    wip

--- via/views/__init__.py
+++ via/views/__init__.py
@@ -1,4 +1,6 @@
 """The views for the Pyramid app."""
+from pyramid.view import view_config
+
 from via.resources import URLResource
 
 
@@ -8,9 +10,15 @@ def add_routes(config):
     config.add_route("view_pdf", "/pdf", factory=URLResource)
     config.add_route("route_by_content", "/route", factory=URLResource)
     config.add_route("debug_headers", "/debug/headers")
+    config.add_route("foo", "/{fizzle:.*}")
 
 
 def includeme(config):
     """Pyramid config."""
     add_routes(config)
     config.scan(__name__)
+
+
+@view_config(route_name="foo", renderer="via:templates/foo.html.jinja2")
+def foo(request):
+    return {"src": request.route_url("route_by_content", _query={"url": request.path[1:]})}
