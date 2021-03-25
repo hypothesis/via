7b6cd703d65e5a68b8647f6c3e99ca7c08d5c768
Sean Hammond <seanh@users.noreply.github.com>
2021-03-25 18:57:02 +0000

    Alarm if requests to Checkmate are failing
    See:
    
    https://github.com/hypothesis/checkmate/issues/190
    https://hypothes-is.slack.com/archives/CR3E3S7K8/p1616692590056200?thread_ts=1616581242.031600&cid=CR3E3S7K8

--- via/views/status.py
+++ via/views/status.py
@@ -1,9 +1,25 @@
-"""Monitoring views."""
+import logging
 
-from pyramid import view
+from checkmatelib import CheckmateClient, CheckmateException
+from pyramid.view import view_config
+
+logger = logging.getLogger(__name__)
 
 
-@view.view_config(route_name="get_status", renderer="json")
-def get_status(_request):
-    """Status endpoint."""
-    return {"status": "okay"}
+@view_config(route_name="status", renderer="json")
+def status(request):
+    settings = request.registry.settings
+
+    checkmate = CheckmateClient(
+        settings["checkmate_url"], settings["checkmate_api_key"]
+    )
+
+    status_ = "okay"
+
+    try:
+        checkmate.check_url("https://example.com/")
+    except CheckmateException:
+        status_ = "checkmate_failure"
+        logger.exception("Checkmate request failed")
+
+    return {"status": status_}
