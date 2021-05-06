81d8abfd533e9b40e246ff469a2481db5e94430d
Robert Knight <robertknight@gmail.com>
2021-05-06 11:04:51 +0100

    Convert `proxy` view to use the `url_from_user_input` helper
    Convert `proxy` view to use the same normalization of "URL from a user
    input source" that will be used for the index page.

--- tests/unit/via/views/proxy_test.py
+++ tests/unit/via/views/proxy_test.py
@@ -11,7 +11,6 @@ class TestProxy:
         [
             ("/https://example.com/foo", "https://example.com/foo"),
             ("/http://example.com/foo", "http://example.com/foo"),
-            ("/example.com/foo", "https://example.com/foo"),
             (
                 "/https://example.com/foo?bar=gar&har=jar#car",
                 "https://example.com/foo?bar=gar&har=jar#car",
@@ -31,8 +30,28 @@ class TestProxy:
         )
         assert result == {"src": via_client_service.url_for.return_value}
 
+    def test_it_normalizes_url(
+        self, pyramid_request, get_url_details, via_client_service, url_from_user_input
+    ):
+        pyramid_request.path = "/https://example.org"
+        url_from_user_input.side_effect = ["https://normalized.com"]
+
+        proxy(pyramid_request)
+
+        url_from_user_input.assert_called_with("https://example.org")
+        get_url_details.assert_called_once_with("https://normalized.com")
+        via_client_service.url_for.assert_called_once_with(
+            "https://normalized.com", sentinel.mime_type, pyramid_request.params
+        )
+
     @pytest.fixture(autouse=True)
     def get_url_details(self, patch):
         get_url_details = patch("via.views.proxy.get_url_details")
         get_url_details.return_value = (sentinel.mime_type, sentinel.status_code)
         return get_url_details
+
+    @pytest.fixture(autouse=True)
+    def url_from_user_input(self, patch):
+        url_from_user_input = patch("via.views.proxy.url_from_user_input")
+        url_from_user_input.side_effect = lambda url: url
+        return url_from_user_input
--- via/views/proxy.py
+++ via/views/proxy.py
@@ -2,16 +2,13 @@ from pyramid.view import view_config
 
 from via.get_url import get_url_details
 from via.services import ViaClientService
+from via.views._helpers import url_from_user_input
 
 
 @view_config(route_name="proxy", renderer="via:templates/proxy.html.jinja2")
 def proxy(request):
-    url = request.path[1:]  # Strip the leading "/" from the path.
-
-    # Let people just type in "example.com" or link to
-    # via.hypothes.is/example.com and have that proxy https://example.com.
-    if not (url.startswith("http://") or url.startswith("https://")):
-        url = "https://" + url
+    # Strip leading '/' and normalize URL.
+    url = url_from_user_input(request.path[1:])
 
     mime_type, _status_code = get_url_details(url)
 
