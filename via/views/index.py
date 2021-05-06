b9926ba82172fd7a709a2acb8775f836706c82c8
Robert Knight <robertknight@gmail.com>
2021-05-06 11:09:15 +0100

    Fix handling of query params and whitespace on the index page
     - Use the `url_from_user_input` helper to process the input URL. This
       ensures that extraneous whitespace is ignored.
    
     - Fix generation of `proxy` route URLs if the input URL contains query
       params. Only the scheme, hostname and path parts of the input URL
       should be passed as the `url` param. The query params need to be
       extracted and passed separately, otherwise Pyramid will URL-escape
       the `?` that begins the query string, resulting in URLs such as
       https://via.hypothes.is/http://example.com%3Fparam=value instead of
       https://via.hypothes.is/http://example.com?param=value.
    
    Fixes #479
    Fixes #482

--- via/views/index.py
+++ via/views/index.py
@@ -1,5 +1,10 @@
+from urllib.parse import urlparse
+
 from pyramid.httpexceptions import HTTPFound, HTTPNotFound
 from pyramid.view import view_config, view_defaults
+
+from via.views._helpers import url_from_user_input
+from via.views.exceptions import BadURL
 
 
 @view_defaults(route_name="index")
@@ -22,8 +27,15 @@ class IndexViews:
         if not self.enabled:
             return HTTPNotFound()
 
+        url = url_from_user_input(self.request.params.get("url", ""))
+        try:
+            parsed = urlparse(url)
+        except ValueError as ex:
+            raise BadURL(url) from ex
+        url_without_query = parsed._replace(query="", fragment="").geturl()
+
         return HTTPFound(
             self.request.route_url(
-                route_name="proxy", url=self.request.params.get("url", "")
+                route_name="proxy", url=url_without_query, _query=parsed.query
             )
         )
