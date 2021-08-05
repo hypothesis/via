3335c7672053364b2f3be9fe3a25cddd786d6221
Jon Betts <jon.betts@hypothes.is>
2021-08-05 14:24:46 +0100

    Add checkmate checking to the proxy end-point
    Also remove re-testing of the `url_from_user_input` method

--- tests/unit/via/views/proxy_test.py
+++ tests/unit/via/views/proxy_test.py
@@ -6,52 +6,38 @@ from via.views.proxy import proxy
 
 
 class TestProxy:
-    @pytest.mark.parametrize(
-        "path,url_to_proxy",
-        [
-            ("/https://example.com/foo", "https://example.com/foo"),
-            ("/http://example.com/foo", "http://example.com/foo"),
-            (
-                "/https://example.com/foo?bar=gar&har=jar#car",
-                "https://example.com/foo?bar=gar&har=jar#car",
-            ),
-        ],
-    )
     def test_it(
-        self, pyramid_request, path, url_to_proxy, get_url_details, via_client_service
+        self,
+        pyramid_request,
+        get_url_details,
+        via_client_service,
+        url_from_user_input,
+        raise_if_blocked,
     ):
-        pyramid_request.path = path
+        pyramid_request.path = "/https://example.org"
 
         result = proxy(pyramid_request)
 
-        get_url_details.assert_called_once_with(url_to_proxy)
+        url_from_user_input.assert_called_with("https://example.org")
+        raise_if_blocked.assert_called_once_with(
+            pyramid_request, url_from_user_input.return_value
+        )
+        get_url_details.assert_called_once_with(url_from_user_input.return_value)
         via_client_service.url_for.assert_called_once_with(
-            url_to_proxy, sentinel.mime_type, pyramid_request.params
+            url_from_user_input.return_value, sentinel.mime_type, pyramid_request.params
         )
         assert result == {"src": via_client_service.url_for.return_value}
-
-    def test_it_normalizes_url(
-        self, pyramid_request, get_url_details, via_client_service, url_from_user_input
-    ):
-        pyramid_request.path = "/https://example.org"
-        url_from_user_input.side_effect = ["https://normalized.com"]
-
-        proxy(pyramid_request)
-
-        url_from_user_input.assert_called_with("https://example.org")
-        get_url_details.assert_called_once_with("https://normalized.com")
-        via_client_service.url_for.assert_called_once_with(
-            "https://normalized.com", sentinel.mime_type, pyramid_request.params
-        )
 
     @pytest.fixture(autouse=True)
     def get_url_details(self, patch):
         get_url_details = patch("via.views.proxy.get_url_details")
-        get_url_details.return_value = (sentinel.mime_type, sentinel.status_code)
+        get_url_details.return_value = sentinel.mime_type, sentinel.status_code
         return get_url_details
 
     @pytest.fixture(autouse=True)
     def url_from_user_input(self, patch):
-        url_from_user_input = patch("via.views.proxy.url_from_user_input")
-        url_from_user_input.side_effect = lambda url: url
-        return url_from_user_input
+        return patch("via.views.proxy.url_from_user_input")
+
+    @pytest.fixture(autouse=True)
+    def raise_if_blocked(self, patch):
+        return patch("via.views.proxy.raise_if_blocked")
