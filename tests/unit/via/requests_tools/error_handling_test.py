78495c7c8a309e86be2aba069dbcaa812773ed1e
Jon Betts <jon.betts@hypothes.is>
2021-09-01 15:09:45 +0100

    Add extra tools for working with streaming data from requests

--- tests/unit/via/requests_tools/error_handling_test.py
+++ tests/unit/via/requests_tools/error_handling_test.py
@@ -2,24 +2,23 @@ import pytest
 from requests import exceptions
 
 from via.exceptions import BadURL, UnhandledException, UpstreamServiceError
-from via.requests_tools.error_handling import handle_errors
+from via.requests_tools.error_handling import handle_errors, iter_handle_errors
+
+EXCEPTION_MAP = (
+    (exceptions.MissingSchema, BadURL),
+    (exceptions.InvalidSchema, BadURL),
+    (exceptions.InvalidURL, BadURL),
+    (exceptions.URLRequired, BadURL),
+    (exceptions.ConnectionError, UpstreamServiceError),
+    (exceptions.Timeout, UpstreamServiceError),
+    (exceptions.TooManyRedirects, UpstreamServiceError),
+    (exceptions.SSLError, UpstreamServiceError),
+    (exceptions.UnrewindableBodyError, UnhandledException),
+)
 
 
 class TestHandleErrors:
-    @pytest.mark.parametrize(
-        "request_exception,expected_exception",
-        (
-            (exceptions.MissingSchema, BadURL),
-            (exceptions.InvalidSchema, BadURL),
-            (exceptions.InvalidURL, BadURL),
-            (exceptions.URLRequired, BadURL),
-            (exceptions.ConnectionError, UpstreamServiceError),
-            (exceptions.Timeout, UpstreamServiceError),
-            (exceptions.TooManyRedirects, UpstreamServiceError),
-            (exceptions.SSLError, UpstreamServiceError),
-            (exceptions.UnrewindableBodyError, UnhandledException),
-        ),
-    )
+    @pytest.mark.parametrize("request_exception,expected_exception", EXCEPTION_MAP)
     def test_it_catches_requests_exceptions(
         self, raiser, request_exception, expected_exception
     ):
@@ -37,3 +36,25 @@ class TestHandleErrors:
             raise exception
 
         return raiser
+
+
+class TestIterHandleErrors:
+    @pytest.mark.parametrize("request_exception,expected_exception", EXCEPTION_MAP)
+    def test_it_catches_requests_exceptions(
+        self, iter_raiser, request_exception, expected_exception
+    ):
+        with pytest.raises(expected_exception):
+            list(iter_raiser(request_exception("Oh noe")))
+
+    def test_it_does_not_catch_regular_exceptions(self, iter_raiser):
+        with pytest.raises(ValueError):
+            list(iter_raiser(ValueError()))
+
+    @pytest.fixture
+    def iter_raiser(self):
+        @iter_handle_errors
+        def iter_raiser(exception):
+            yield 1
+            raise exception
+
+        return iter_raiser
--- via/requests_tools/__init__.py
+++ via/requests_tools/__init__.py
@@ -1,2 +1,3 @@
 from via.requests_tools.error_handling import handle_errors
 from via.requests_tools.headers import add_request_headers, clean_headers
+from via.requests_tools.streaming import stream_bytes
