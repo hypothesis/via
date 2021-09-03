17c2a0e6d9a150231837e3a6a622030f265849ca
Jon Betts <jon.betts@hypothes.is>
2021-09-03 15:33:49 +0100

    Fix the route URL to include query parameters

--- via/views/proxy.py
+++ via/views/proxy.py
@@ -1,3 +1,4 @@
+from h_vialib.configuration import Configuration
 from pyramid.view import view_config
 
 from via.get_url import get_url_details
@@ -8,7 +9,8 @@ from via.views._helpers import url_from_user_input
 @view_config(route_name="proxy", renderer="via:templates/proxy.html.jinja2")
 def proxy(request):
     # Strip leading '/' and normalize URL.
-    url = url_from_user_input(request.path[1:])
+    url = url_from_user_input(request.path_qs[1:])
+    url = Configuration.strip_from_url(url)
 
     request.checkmate.raise_if_blocked(url)
 
