bc9b5044a49d5a5641125c7d59ee49498f06b1c7
Jon Betts <jon.betts@hypothes.is>
2021-09-03 15:33:49 +0100

    Fix the route URL to include query parameters

--- tests/unit/via/views/proxy_test.py
+++ tests/unit/via/views/proxy_test.py
@@ -6,20 +6,16 @@ from via.views.proxy import proxy
 
 
 class TestProxy:
-    def test_it(
-        self, pyramid_request, get_url_details, via_client_service, url_from_user_input
-    ):
-        pyramid_request.path = "/https://example.org"
+    def test_it(self, pyramid_request, get_url_details, via_client_service):
+        pyramid_request.path_qs = "/https://example.org?a=1&via.sec=HEX"
 
         result = proxy(pyramid_request)
 
-        url_from_user_input.assert_called_with("https://example.org")
-        pyramid_request.checkmate.raise_if_blocked.assert_called_once_with(
-            url_from_user_input.return_value
-        )
-        get_url_details.assert_called_once_with(url_from_user_input.return_value)
+        url = "https://example.org?a=1"
+        pyramid_request.checkmate.raise_if_blocked.assert_called_once_with(url)
+        get_url_details.assert_called_once_with(url)
         via_client_service.url_for.assert_called_once_with(
-            url_from_user_input.return_value, sentinel.mime_type, pyramid_request.params
+            url, sentinel.mime_type, pyramid_request.params
         )
         assert result == {"src": via_client_service.url_for.return_value}
 
@@ -28,7 +24,3 @@ class TestProxy:
         get_url_details = patch("via.views.proxy.get_url_details")
         get_url_details.return_value = sentinel.mime_type, sentinel.status_code
         return get_url_details
-
-    @pytest.fixture(autouse=True)
-    def url_from_user_input(self, patch):
-        return patch("via.views.proxy.url_from_user_input")
