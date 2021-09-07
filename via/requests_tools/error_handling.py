ada065f64460e0596ec12742ddbe5ce130d7e94c
Jon Betts <jon.betts@hypothes.is>
2021-09-07 15:10:48 +0100

    Add logging to the iter handler
    This is a bit weird, but it's only used by the Google end-point where
    we would like some more logging. If we wanted to do this properly
    this would be an arg to the decorator. But currently that arg would
    always be True so why bother.

--- via/requests_tools/error_handling.py
+++ via/requests_tools/error_handling.py
@@ -1,6 +1,7 @@
 """Helpers for capturing requests exceptions."""
 
 from functools import wraps
+from logging import getLogger
 
 from requests import exceptions
 
@@ -22,19 +23,47 @@ ERROR_MAP = {
 }
 
 
-def map_exception(exception):
-    def _map_exception(exception):
+LOG = getLogger(__name__)
+
+
+def _summarise_requests_exception(
+    exception: exceptions.RequestException, error_message
+):
+    """Create a summary string from details from a `RequestsException`."""
+    url = f"'{exception.request.url}'" if exception.request else "unknown URL"
+    status = (
+        "no response"
+        if exception.response is None
+        else f"{exception.response.status_code}: '{exception.response.reason}'"
+    )
+    return f"Requests exception - {type(exception)} Got {status} from {url} ({error_message})"
+
+
+def _map_exception(exception, log_errors=False):
+    """Map a child of RequestsException to our exceptions.
+
+    :param exception: Exception to map
+    :param log_errors: Also log about this error
+    :return: One of our exceptions or None if we don't want to map it
+    """
+
+    def _map(exception):
         for exc_class, mapped_class in ERROR_MAP.items():
             if isinstance(exception, exc_class):
                 return mapped_class
 
         return None
 
-    mapped_class = _map_exception(exception)
+    mapped_class = _map(exception)
     if not mapped_class:
         return None
 
-    return mapped_class(exception.args[0] if exception.args else None)
+    error_message = exception.args[0] if exception.args else None
+
+    if log_errors:
+        LOG.error(_summarise_requests_exception(exception, error_message))
+
+    return mapped_class(error_message)
 
 
 def handle_errors(inner):
@@ -46,7 +75,7 @@ def handle_errors(inner):
             return inner(*args, **kwargs)
 
         except Exception as err:
-            if mapped := map_exception(err):
+            if mapped := _map_exception(err):
                 raise mapped from err
 
             raise
@@ -63,7 +92,7 @@ def iter_handle_errors(inner):
             yield from inner(*args, **kwargs)
 
         except Exception as err:
-            if mapped := map_exception(err):
+            if mapped := _map_exception(err, log_errors=True):
                 raise mapped from err
 
             raise
