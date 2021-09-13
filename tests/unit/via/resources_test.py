c3d7615ed366f11cb0fe74cd5e060ebf3fd241f5
Jon Betts <jon.betts@hypothes.is>
2021-09-13 14:23:13 +0100

    Merge `url_from_user_input` into the URLResource
    This is sort of a best of both. URLResource has gained the normalisation
    from `url_from_user_input` and the reverse has gained the error handling
    in `URLResource`.

--- tests/unit/via/resources_test.py
+++ tests/unit/via/resources_test.py
@@ -3,18 +3,52 @@ from pyramid.httpexceptions import HTTPBadRequest
 
 from via.resources import URLResource
 
+NORMALISED_URLS = [
+    # URLs that should be returned unchanged
+    ("http://example.com", "http://example.com"),
+    ("https://example.com", "https://example.com"),
+    ("http://example.com?a=1", "http://example.com?a=1"),
+    # URLs without a protocol that should have `https://` prefixed
+    ("example.com", "https://example.com"),
+    # Leading and trailing whitespace that should be stripped
+    (" http://example.com", "http://example.com"),
+    ("http://example.com ", "http://example.com"),
+    # Check we remove Via stuff
+    ("http://example.com?a=1&via.sec=TOKEN", "http://example.com?a=1"),
+]
+
 
 class TestURLResource:
-    def test_it_returns_url(self, pyramid_request):
-        url = pyramid_request.params["url"] = "http://example.com"
+    @pytest.mark.parametrize("url,expected", NORMALISED_URLS)
+    def test_url_from_query_returns_url(self, pyramid_request, url, expected):
+        pyramid_request.params["url"] = url
         context = URLResource(pyramid_request)
 
-        assert context.url() == url
+        assert context.url_from_query() == expected
 
     @pytest.mark.parametrize("params", ({}, {"urk": "foo"}, {"url": ""}))
-    def test_it_raises_HTTPBadRequest_for_bad_urls(self, params, pyramid_request):
+    def test_url_from_query_raises_HTTPBadRequest_for_bad_urls(
+        self, params, pyramid_request
+    ):
         pyramid_request.params = params
         context = URLResource(pyramid_request)
 
         with pytest.raises(HTTPBadRequest):
-            context.url()
+            context.url_from_query()
+
+    @pytest.mark.parametrize("url,expected", NORMALISED_URLS)
+    def test_url_from_path_returns_url(self, pyramid_request, url, expected):
+        pyramid_request.path_qs = f"/{url}"
+        context = URLResource(pyramid_request)
+
+        assert context.url_from_path() == expected
+
+    @pytest.mark.parametrize("bad_path", ("/", "/  "))
+    def test_url_from_path_raises_HTTPBadRequest_for_missing_urls(
+        self, pyramid_request, bad_path
+    ):
+        pyramid_request.path_qs = bad_path
+        context = URLResource(pyramid_request)
+
+        with pytest.raises(HTTPBadRequest):
+            context.url_from_path()
--- via/resources.py
+++ via/resources.py
@@ -1,7 +1,6 @@
 """Context objects for views."""
+from h_vialib import Configuration
 from pyramid.httpexceptions import HTTPBadRequest
-
-# pylint: disable=too-few-public-methods
 
 
 class URLResource:
@@ -10,18 +9,43 @@ class URLResource:
     def __init__(self, request):
         self._request = request
 
-    def url(self):
-        """Get the 'url' parameter.
+    def url_from_query(self):
+        """Get the 'url' parameter from the query.
 
         :return: The URL as a string
         :raise HTTPBadRequest: If the url is missing or empty
         """
         try:
-            url = self._request.params["url"]
+            url = self._request.params["url"].strip()
         except KeyError as err:
             raise HTTPBadRequest("Required parameter 'url' missing") from err
 
         if not url:
             raise HTTPBadRequest("Required parameter 'url' is blank")
 
-        return url
+        return self._normalise_url(url)
+
+    def url_from_path(self):
+        """Get the 'url' parameter from the path.
+
+        :return: The URL as a string
+        :raise HTTPBadRequest: If the url is missing or empty
+        """
+
+        url = self._request.path_qs[1:].strip()
+        if not url:
+            raise HTTPBadRequest("Required path part 'url` is missing")
+
+        return self._normalise_url(url)
+
+    @classmethod
+    def _normalise_url(cls, url):
+        """Return a normalized URL from user input.
+
+        Take a URL from user input (eg. a URL input field or path parameter) and
+        convert it to a normalized form.
+        """
+        if not (url.startswith("http://") or url.startswith("https://")):
+            url = "https://" + url
+
+        return Configuration.strip_from_url(url)
