d457f981dd9530621b7c1c71a06b7df8b7d587db
Jon Betts <jon.betts@hypothes.is>
2021-09-13 14:22:16 +0100

    Add the context to the URL related views and stop testing it
    The tests are super low value and just ammount to copying the code

--- via/views/__init__.py
+++ via/views/__init__.py
@@ -2,9 +2,10 @@
 from via.resources import URLResource
 
 
-def add_routes(config):
+def add_routes(config):  # pragma: no cover
     """Add routes to pyramid config."""
-    config.add_route("index", "/")
+
+    config.add_route("index", "/", factory=URLResource)
     config.add_route("get_status", "/_status")
     config.add_route("view_pdf", "/pdf", factory=URLResource)
     config.add_route("route_by_content", "/route", factory=URLResource)
@@ -14,10 +15,11 @@ def add_routes(config):
         "proxy_google_drive_file:resource_key",
         "/google_drive/{file_id}/{resource_key}/proxied.pdf",
     )
-    config.add_route("proxy", "/{url:.*}")
+    config.add_route("proxy", "/{url:.*}", factory=URLResource)
 
 
-def includeme(config):
+def includeme(config):  # pragma: no cover
     """Pyramid config."""
+
     add_routes(config)
     config.scan(__name__)
