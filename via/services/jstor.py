71314c8776c35da2c33e6eaf634028e4c05d80d8
Jon Betts <jon.betts@hypothes.is>
2022-05-05 14:29:53 +0100

    Add streaming Base64 decoding

--- via/services/jstor.py
+++ via/services/jstor.py
@@ -1,3 +1,5 @@
+import base64
+
 from via.requests_tools.headers import add_request_headers
 from via.services import HTTPService
 
@@ -36,9 +38,50 @@ class JSTORAPI:
             doi = f"{self.DEFAULT_DOI_PREFIX}/{doi}"
 
         url = f"{self._api_url}/{doi}?ip={jstor_ip}"
-        return self._http.stream(
+        stream = self._http.stream(
             url, headers=add_request_headers({"Accept": "application/pdf"})
         )
+
+        # Currently, JSTOR is sending us a stream of base 64 encoded data. This
+        # isn't intentional and may change if they can fix it. For the time
+        # being we need to decode this on the fly. This is both because it
+        # gives the user a better time to first byte, but also because the
+        # machinery that consumes this expects a stream, not a string.
+        return decode_base64_stream(stream)
+
+
+def decode_base64_stream(b64_stream):
+    """Get a stream of decoded bytes from an input stream of base 64 bytes."""
+
+    unprocessed = b""
+
+    for chunk in b64_stream:
+        # Take a chunk, but remove new lines which aren't part of the b64 data
+        unprocessed += chunk.replace(b"\n", b"")
+
+        # Every 4 bytes of Base 64 encode 3 octets exactly; any more or less
+        # will potentially encode a partial octet. Therefore, we need to split
+        # on exactly 4 bytes:
+        # +-----------+-----------+-----------+-----------+
+        # |    T      |     W     |     F     |     u     | 4 bytes of 64 input
+        # |0 1 0 0 1 1|0 1|0 1 1 0|0 0 0 1|0 1|1 0 1 1 1 0|
+        # |       M       |       a       |       n       | 3 output octets
+        # +---------------+---------------+---------------+
+        safe_len = 4 * (len(unprocessed) // 4)
+
+        # We'll take that many off of the front of the unprocessed bytes
+        to_process, unprocessed = unprocessed[:safe_len], unprocessed[safe_len:]
+
+        # It could be we got a very small chunk, and there aren't enough bytes
+        # to process
+        if to_process:
+            yield base64.b64decode(to_process)
+
+    # Clear up any remaining and add extra padding to ensure we can decode
+    # regardless of the length. The b64 module doesn't care if there is too
+    # much padding, but it might care about too little
+    if unprocessed:
+        yield base64.b64decode(unprocessed + b"====")
 
 
 def factory(_context, request):
