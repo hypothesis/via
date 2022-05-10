e0f8f4dc2184cd1be2490dec33ace463c795014b
Jon Betts <jon.betts@hypothes.is>
2022-05-10 13:35:17 +0100

    Swap to the new end-point for JSTOR which returns S3 content
    I'm not sure this is any faster for us, as two calls are worse than
    one, but it does remove all the Base64 stuff. This also leaves the
    possibility of doing the translation to give the URL before we pass
    to the view, which might remove some more code.

--- via/services/jstor.py
+++ via/services/jstor.py
@@ -1,8 +1,8 @@
-import base64
 from datetime import datetime, timedelta
 
 from jose import jwt
 
+from via.exceptions import UpstreamServiceError
 from via.requests_tools.headers import add_request_headers
 from via.services import HTTPService
 
@@ -35,27 +35,31 @@ class JSTORAPI:
 
         :param url: The URL to stream
         :param site_code: The code we use to authenticate ourselves to JSTOR
+        :raises UpstreamServiceError: If we get bad data back from the service
         """
 
+        # Get a signed S3 URL from JSTOR which expires after 10 minutes, and
+        # start a new request to stream that content
+        return self._http.stream(url=self._get_signed_s3_url(url, site_code))
+
+    def _get_signed_s3_url(self, url, site_code):
         doi = url.replace("jstor://", "")
         if "/" not in doi:
             doi = f"{self.DEFAULT_DOI_PREFIX}/{doi}"
 
         token = self._get_access_token(site_code)
+        s3_url = self._http.request(
+            method="GET",
+            url=f"{self._api_url}/pdf-url/{doi}",
+            headers=add_request_headers({"Authorization": f"Bearer {token}"}),
+        ).text
 
-        stream = self._http.stream(
-            url=f"{self._api_url}/pdf/{doi}",
-            headers=add_request_headers(
-                {"Accept": "application/pdf", "Authorization": f"Bearer {token}"}
-            ),
-        )
+        if not s3_url.startswith("https://"):
+            raise UpstreamServiceError(
+                f"Expected to get an S3 URL but got: {s3_url} instead"
+            )
 
-        # Currently, JSTOR is sending us a stream of base 64 encoded data. This
-        # isn't intentional and may change if they can fix it. For the time
-        # being we need to decode this on the fly. This is both because it
-        # gives the user a better time to first byte, but also because the
-        # machinery that consumes this expects a stream, not a string.
-        return decode_base64_stream(stream)
+        return s3_url
 
     def _get_access_token(self, site_code):
         return jwt.encode(
@@ -66,40 +70,6 @@ class JSTORAPI:
             self._secret,
             algorithm="HS256",
         )
-
-
-def decode_base64_stream(b64_stream):
-    """Get a stream of decoded bytes from an input stream of base 64 bytes."""
-
-    unprocessed = b""
-
-    for chunk in b64_stream:
-        # Take a chunk, but remove new lines which aren't part of the b64 data
-        unprocessed += chunk.replace(b"\n", b"")
-
-        # Every 4 bytes of Base 64 encode 3 octets exactly; any more or less
-        # will potentially encode a partial octet. Therefore, we need to split
-        # on exactly 4 bytes:
-        # +-----------+-----------+-----------+-----------+
-        # |    T      |     W     |     F     |     u     | 4 bytes of 64 input
-        # |0 1 0 0 1 1|0 1|0 1 1 0|0 0 0 1|0 1|1 0 1 1 1 0|
-        # |       M       |       a       |       n       | 3 output octets
-        # +---------------+---------------+---------------+
-        safe_len = 4 * (len(unprocessed) // 4)
-
-        # We'll take that many off of the front of the unprocessed bytes
-        to_process, unprocessed = unprocessed[:safe_len], unprocessed[safe_len:]
-
-        # It could be we got a very small chunk, and there aren't enough bytes
-        # to process
-        if to_process:
-            yield base64.b64decode(to_process)
-
-    # Clear up any remaining and add extra padding to ensure we can decode
-    # regardless of the length. The b64 module doesn't care if there is too
-    # much padding, but it might care about too little
-    if unprocessed:
-        yield base64.b64decode(unprocessed + b"====")
 
 
 def factory(_context, request):
