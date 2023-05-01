38cdd8d9e8db934e94910e8e17643c4114605c16
Robert Knight <robertknight@gmail.com>
2023-05-01 15:16:36 +0100

    Add frontend unit testing infrastructure
    Add frontend unit testing infrastructure using our standard stack (Karma, mocha,
    chai, sinon, Enzyme etc.)
    
    Some notes:
    
     - We don't have any CommonJS runtime dependencies, but some of our test deps
       are still CJS, so we still need Rollup's CJS plugin for those.
    
     - Tests produce a warning about a `build/styles/*.css` pattern in
       karma.config.js not matching any files. This is because the video player
       doesn't have any CSS yet. That will change soon.
    
     - The setup file for the test environment was named `setup-tests.js` in this
       project rather than `bootstrap.js` as in other projects. I feel that the
       bootstrap name is completely non-obvious and we should change it.

---
+++ via/static/scripts/setup-tests.js
@@ -0,0 +1,34 @@
+import { configure } from 'enzyme';
+import { Adapter } from 'enzyme-adapter-preact-pure';
+import 'preact/debug';
+
+// Expose sinon assertions.
+sinon.assert.expose(assert, { prefix: null });
+
+// Configure Enzyme for UI tests.
+configure({ adapter: new Adapter() });
+
+// Ensure that uncaught exceptions between tests result in the tests failing.
+// This works around an issue with mocha / karma-mocha, see
+// https://github.com/hypothesis/client/issues/2249.
+let pendingError = null;
+let pendingErrorNotice = null;
+
+/* istanbul ignore next */
+window.addEventListener('error', event => {
+  pendingError = event.error;
+  pendingErrorNotice = 'An uncaught exception was thrown between tests';
+});
+/* istanbul ignore next */
+window.addEventListener('unhandledrejection', event => {
+  pendingError = event.reason;
+  pendingErrorNotice = 'An uncaught promise rejection occurred between tests';
+});
+
+afterEach(() => {
+  /* istanbul ignore if */
+  if (pendingError) {
+    console.error(pendingErrorNotice);
+    throw pendingError;
+  }
+});
