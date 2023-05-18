58f4261c3a1bbfab778bda6b54e1702f3c896d12
Robert Knight <robertknight@gmail.com>
2023-05-18 13:23:26 +0100

    Add functions for filtering transcript with user-provided query

---
+++ via/static/scripts/video_player/utils/test/transcript-test.js
@@ -0,0 +1,20 @@
+import { sampleTranscript } from '../../sample-transcript';
+import { filterTranscript } from '../transcript';
+
+describe('filterTranscript', () => {
+  it('returns matching segments and offsets', () => {
+    const query = 'Nintendo 64';
+
+    const segmentMatches = filterTranscript(sampleTranscript, query);
+
+    assert.equal(segmentMatches.size, 5);
+    for (const [index, matches] of segmentMatches.entries()) {
+      const text = sampleTranscript[index].text;
+      assert.include(text.toLowerCase(), query.toLowerCase());
+      assert.notEqual(matches.length, 0);
+      for (const { start, end } of matches) {
+        assert.equal(text.slice(start, end).toLowerCase(), query.toLowerCase());
+      }
+    }
+  });
+});
