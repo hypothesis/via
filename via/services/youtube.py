e3ea5ff34d86e485457016c1706b74d10ccce9d7
Sean Hammond <seanh@users.noreply.github.com>
2023-06-07 18:15:11 +0100

    Add YouTube URL parsing
    To extract the video ID from a YouTube URL. Nothing calls this yet.

--- via/services/youtube.py
+++ via/services/youtube.py
@@ -1,3 +1,6 @@
+from urllib.parse import parse_qs, urlparse
+
+
 class YouTubeService:
     def __init__(self, enabled: bool):
         self._enabled = enabled
@@ -5,6 +8,36 @@ class YouTubeService:
     @property
     def enabled(self):
         return self._enabled
+
+    def get_video_id(self, url):
+        """Return the YouTube video ID from the given URL, or None."""
+        parsed = urlparse(url)
+        path_parts = parsed.path.split("/")
+
+        # youtu.be/VIDEO_ID
+        if parsed.netloc in "youtu.be" and len(path_parts) >= 2 and not path_parts[0]:
+            return path_parts[1]
+
+        if parsed.netloc not in ["www.youtube.com", "youtube.com"]:
+            return None
+
+        query_params = parse_qs(parsed.query)
+
+        # https://youtube.com?v=VIDEO_ID, youtube.com/watch?v=VIDEO_ID, etc.
+        if "v" in query_params:
+            return query_params["v"][0]
+
+        path_parts = parsed.path.split("/")
+
+        # https://yotube.com/v/VIDEO_ID, /embed/VIDEO_ID, etc.
+        if (
+            len(path_parts) >= 3
+            and not path_parts[0]
+            and path_parts[1] in ["v", "embed", "shorts", "live"]
+        ):
+            return path_parts[2]
+
+        return None
 
 
 def factory(_context, request):
