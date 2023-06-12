c109011beb68e377bb26fe5d65d7ab21b2427472
Sean Hammond <seanh@users.noreply.github.com>
2023-06-12 13:05:37 +0100

    Use YouTube URLs not video IDs in the /video route
    Change Via's `/video` to accept full YouTube URLs in a `url` query param
    instead of accepting just YouTube video IDs in a URL path part.
    
    This is because the `/video` endpoint may need the full YouTube URL not
    just the video ID:
    
    * The `/video` endpoint may need to pass the full YouTube URL to
      Checkmate to check whether the video has been blocked
    * The `/video` endpoint may need the full YouTube URL for unforeseen
      functionality reasons. For example we may find out in future that our
      video player needs to handle YouTube `/live/` URLs differently from
      `/watch` ones.
    
    This commit also makes `/video` show an error page if the given URL is
    not a YouTube URL.
    
    Known issue: `/video` crashes if there's no `url` query param.
    
    [Slack thread](https://hypothes-is.slack.com/archives/C1MA4E9B9/p1686563922909299)

--- tests/unit/via/views/view_video_test.py
+++ tests/unit/via/views/view_video_test.py
@@ -3,13 +3,14 @@ from unittest.mock import sentinel
 import pytest
 from pyramid.httpexceptions import HTTPUnauthorized
 
+from via.exceptions import BadURL
 from via.views.view_video import view_video
 
 
 @pytest.mark.usefixtures("youtube_service")
 class TestViewVideo:
-    def test_it(self, pyramid_request, Configuration):
-        pyramid_request.matchdict["id"] = "abcdef"
+    def test_it(self, pyramid_request, Configuration, youtube_service):
+        youtube_service.get_video_id.return_value = sentinel.video_id
 
         response = view_video(pyramid_request)
 
@@ -30,7 +31,18 @@ class TestViewVideo:
                 },
             ],
         }
-        assert response["video_id"] == "abcdef"
+        assert response["video_id"] == sentinel.video_id
+
+    def test_it_errors_if_the_url_is_invalid(self, pyramid_request, youtube_service):
+        # YouTubeService returns None if it can't extract the YouTube video ID
+        # from the URL, which happens if the URL doesn't match a YouTube video
+        # URL format that YouTubeService supports.
+        youtube_service.get_video_id.return_value = None
+
+        with pytest.raises(BadURL) as exc_info:
+            view_video(pyramid_request)
+
+        assert str(exc_info.value).startswith("Unsupported video URL")
 
     def test_it_with_YouTube_transcripts_disabled(
         self, pyramid_request, youtube_service
@@ -49,3 +61,8 @@ class TestViewVideo:
         )
 
         return Configuration
+
+    @pytest.fixture
+    def pyramid_request(self, pyramid_request):
+        pyramid_request.params["url"] = sentinel.url
+        return pyramid_request
