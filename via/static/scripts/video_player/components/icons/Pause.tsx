a10d28f4ffd4ebffeace8cfb0f273bc6db3bf464
Lyza Danger Gardner <lyza@lyza.com>
2023-06-15 14:18:51 -0400

    Add `viewbox` attribute to SVGs
    This ensures that they won't be cut off if rendered at smaller sizes.

--- via/static/scripts/video_player/components/icons/Pause.tsx
+++ via/static/scripts/video_player/components/icons/Pause.tsx
@@ -13,6 +13,7 @@ export default function PauseIcon(props: PauseIconProps) {
       width="16"
       height="16"
       fill="none"
+      viewBox="0 0 16 16"
       data-component="PauseIcon"
       {...props}
     >
