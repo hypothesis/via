1975471fe5c6c836c56b28255246e449477c87b2
Alejandro Celaya <alejandro.celaya@hypothes.is>
2023-06-20 16:19:23 +0200

    Add missing frontend make tasks

--- frontend.mk
+++ frontend.mk
@@ -13,3 +13,20 @@ build: python node_modules/.uptodate
 $(call help,make update-pdfjs,"update our copy of PDF.js")
 update-pdfjs: python
 	@tox -qe updatepdfjs
+
+.PHONY: frontend-lint
+$(call help,make frontend-lint,"lint the frontend code")
+frontend-lint: node_modules/.uptodate
+	@yarn checkformatting
+	@yarn lint
+	@yarn typecheck
+
+.PHONY: frontend-format
+$(call help,make frontend-format,"format the frontend code")
+frontend-format: node_modules/.uptodate
+	@yarn format
+
+.PHONY: frontend-test
+$(call help,make frontend-test,"run the frontend tests")
+frontend-test: node_modules/.uptodate
+	@yarn test $(ARGS)
