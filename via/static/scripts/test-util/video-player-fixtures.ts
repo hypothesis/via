4f086a340a893f96f521d8bdcd136a8f1d06a60a
Robert Knight <robertknight@gmail.com>
2023-06-22 16:41:07 +0100

    Fetch video transcript from API instead of using hard-coded data
     - Add types for API responses from the server and `callAPI` utility to make
       calls to API.
    
     - Change `VideoPlayerApp` component to accept either a pre-fetched transcript
       or API call configuration as the transcript source. If passed an API call
       config, it displays a loading state, performs the API call and then displays
       the response.
    
       In the actual app an API call is always used, but the pre-fetched transcript
       is useful in tests.
    
     - Change video player entry point to read API index from JSON configuration and
       pass the config for the transcript API call to `VideoPlayerApp`.

---
+++ via/static/scripts/test-util/video-player-fixtures.ts
@@ -0,0 +1,39 @@
+// Shared fixtures for video player app tests.
+
+/**
+ * Configuration for video player app.
+ */
+export const videoPlayerConfig = {
+  api: {
+    transcript: {
+      doc: 'Get the transcript of the current video',
+      url: 'https://dummy-via.hypothes.is/api/youtube/test_video_id',
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer FAKE_JWT_TOKEN',
+      },
+    },
+  },
+};
+
+/**
+ * Dummy response from transcript API endpoint.
+ */
+export const transcriptsAPIResponse = {
+  data: {
+    type: 'transcripts',
+    id: 'test_video_id',
+    attributes: {
+      segments: [
+        {
+          text: '[Music]',
+          start: 0.0,
+        },
+        {
+          text: 'how many of you remember the first time',
+          start: 5.6,
+        },
+      ],
+    },
+  },
+};
