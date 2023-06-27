10dbfba9d8fb57e40634a9696b09a8069c20eef0
Robert Knight <robertknight@gmail.com>
2023-06-27 12:15:41 +0100

    Add `waitForElement` test utility
    This was adapted from code in the hypothesis/lms repo.

--- via/static/scripts/test-util/wait.ts
+++ via/static/scripts/test-util/wait.ts
@@ -7,7 +7,7 @@ export async function waitFor<T>(
   condition: () => T,
   timeout = 10,
   what = condition.toString()
-): Promise<T> {
+): Promise<NonNullable<T>> {
   const result = condition();
   if (result) {
     return result;
@@ -28,6 +28,41 @@ export async function waitFor<T>(
       }
     });
   });
+}
+
+// Minimal Enzyme types needed by `waitForElement`.
+
+// eslint-disable-next-line no-use-before-define
+type Predicate = (wrapper: EnzymeWrapper) => boolean;
+
+type EnzymeWrapper = {
+  length: number;
+  update(): void;
+  find(query: string | Predicate): EnzymeWrapper;
+};
+
+/**
+ * Wait up to `timeout` ms for an element to be rendered.
+ *
+ * @param selector - Selector string or function to pass to `wrapper.find`
+ */
+export function waitForElement(
+  wrapper: EnzymeWrapper,
+  selector: string | Predicate,
+  timeout = 10
+): Promise<EnzymeWrapper> {
+  return waitFor(
+    () => {
+      wrapper.update();
+      const el = wrapper.find(selector);
+      if (el.length === 0) {
+        return null;
+      }
+      return el;
+    },
+    timeout,
+    `"${selector}" to render`
+  );
 }
 
 export function delay(ms: number) {
