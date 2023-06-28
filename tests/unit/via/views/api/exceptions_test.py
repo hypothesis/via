fba381b336421178059cda959654769f926a119b
Sean Hammond <seanh@users.noreply.github.com>
2023-06-28 13:35:36 +0100

    Move API error handling into exception view

--- tests/unit/via/views/api/exceptions_test.py
+++ tests/unit/via/views/api/exceptions_test.py
@@ -1,6 +1,36 @@
+import logging
+
 import pytest
 
 from via.views.api.exceptions import APIExceptionViews
+
+
+def test_exception(views, context, pyramid_request, report_exception_to_sentry, caplog):
+    response = views.exception()
+
+    report_exception_to_sentry.assert_called_once_with(context)
+    assert caplog.record_tuples == [
+        ("via.views.api.exceptions", logging.ERROR, str(context))
+    ]
+    assert pyramid_request.response.status_int == 500
+    assert response == {
+        "errors": [
+            {
+                "status": 500,
+                "code": "RuntimeError",
+                "title": "Something went wrong",
+                "detail": str(context).strip(),
+            }
+        ]
+    }
+
+
+def test_exception_with_a_cause(views, context):
+    context.cause = "The cause of the error"
+
+    response = views.exception()
+
+    assert response["errors"][0]["title"] == context.cause
 
 
 def test_forbidden(views, pyramid_request):
@@ -18,5 +48,15 @@ def test_notfound(views, pyramid_request):
 
 
 @pytest.fixture
-def views(pyramid_request):
-    return APIExceptionViews(pyramid_request)
+def context():
+    return RuntimeError("Test error")
+
+
+@pytest.fixture
+def views(context, pyramid_request):
+    return APIExceptionViews(context, pyramid_request)
+
+
+@pytest.fixture(autouse=True)
+def report_exception_to_sentry(patch):
+    return patch("via.views.api.exceptions.report_exception_to_sentry")
--- via/views/api/exceptions.py
+++ via/views/api/exceptions.py
@@ -1,10 +1,45 @@
-from pyramid.view import forbidden_view_config, notfound_view_config, view_defaults
+import logging
+
+from h_pyramid_sentry import report_exception as report_exception_to_sentry
+from pyramid.view import (
+    exception_view_config,
+    forbidden_view_config,
+    notfound_view_config,
+    view_defaults,
+)
+
+logger = logging.getLogger(__name__)
 
 
 @view_defaults(path_info="^/api/.*", renderer="json")
 class APIExceptionViews:
-    def __init__(self, request):
+    def __init__(self, context, request):
+        self.context = context
         self.request = request
+
+    @exception_view_config(Exception)
+    def exception(self):
+        report_exception_to_sentry(self.context)
+        logger.exception(self.context)
+
+        self.request.response.status_int = 500
+
+        return {
+            "errors": [
+                {
+                    "status": self.request.response.status_int,
+                    "code": self.context.__class__.__name__,
+                    "title": str(
+                        getattr(
+                            self.context,
+                            "cause",
+                            "Something went wrong",
+                        )
+                    ),
+                    "detail": str(self.context).strip(),
+                }
+            ]
+        }
 
     @forbidden_view_config()
     def forbidden(self):
