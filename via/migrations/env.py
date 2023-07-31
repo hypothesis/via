96a50e3f6eb3495b5867637269ccf5a7f4b6db64
Sean Hammond <seanh@users.noreply.github.com>
2023-07-31 14:56:32 +0100

    Fix Alembic auto-generation
    You need to import `via.models` in `env.py` so that Alembic migration
    auto-generation works.

--- via/migrations/env.py
+++ via/migrations/env.py
@@ -4,6 +4,7 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from via import models
 from via.db import Base
 
 # this is the Alembic Config object, which provides
