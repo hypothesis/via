e54c2f636aa66444efa870ed0c4afd89039b742a
Sean Hammond <seanh@users.noreply.github.com>
2023-08-12 20:16:56 +0100

    Return the oldest transcript matching the video ID
    Just return the oldest saved transcript in the DB that matches the given
    `video_id`, instead of also searching for the hard-coded `transcript_id`
    value of `"en"`.

--- tests/factories/transcript.py
+++ tests/factories/transcript.py
@@ -10,20 +10,17 @@ class TranscriptFactory(SQLAlchemyModelFactory):
 
     video_id = Sequence(lambda n: f"video_id_{n}")
     transcript_id = Sequence(lambda n: f"transcript_id_{n}")
-    transcript = [
-        {
-            "text": "[Music]",
-            "start": 0.0,
-            "duration": 7.52,
-        },
-        {
-            "text": "how many of you remember the first time",
-            "start": 5.6,
-            "duration": 4.72,
-        },
-        {
-            "text": "you saw a playstation 1 game if you were",
-            "start": 7.52,
-            "duration": 4.72,
-        },
-    ]
+    transcript = Sequence(
+        lambda n: [
+            {
+                "text": f"This is the first line of transcript {n}",
+                "start": 0.0,
+                "duration": 7.52,
+            },
+            {
+                "text": f"This is the second line of transcript {n}",
+                "start": 5.6,
+                "duration": 4.72,
+            },
+        ]
+    )
