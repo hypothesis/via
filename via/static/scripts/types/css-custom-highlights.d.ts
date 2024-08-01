6a955e792fbb098048c1023b6734f1a6d1f8b828
Robert Knight <robertknight@gmail.com>
2024-08-01 09:30:30 +0100

    Update CSS Highlights types for compatibility with TypeScript 5.5
    TS 5.5 now includes incomplete types for CSS highlights. Change
    `css-custom-highlights.d.ts` to include just methods that are missing but needed
    in our code.

--- via/static/scripts/types/css-custom-highlights.d.ts
+++ via/static/scripts/types/css-custom-highlights.d.ts
@@ -1,19 +1,16 @@
 // Types for the CSS Custom Highlight API.
 //
 // See https://developer.mozilla.org/en-US/docs/Web/API/CSS_Custom_Highlight_API.
+//
+// TypeScript's built-in types includes incomplete versions of these types.
 
-declare class Highlight {
-  constructor(...ranges: Range[]);
+declare interface Highlight {
   add(r: Range): void;
   delete(r: Range): void;
 }
 
-interface HighlightsRegistry {
+interface HighlightRegistry {
   delete(name: string): void;
   get(name: string): Highlight | undefined;
   set(name: string, highlight: Highlight): void;
-}
-
-declare namespace CSS {
-  let highlights: HighlightsRegistry;
 }
