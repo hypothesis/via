82124bff41bb747007805f1718a8c0cad60ab0f0
Sean Hammond <seanh@users.noreply.github.com>
2025-09-24 12:58:35 +0100

    Add Sentry Logs support

--- via/sentry_filters.py
+++ via/sentry_filters.py
@@ -1,6 +1,15 @@
-"""Functions for filtering out events we don't want to report to Sentry.
+"""Functions for filtering out events and log messages we don't want to report to Sentry."""
 
-These are intended to be passed to h_pyramid_sentry.EventFilter.add_filters
-"""
+from sentry_sdk.types import Hint, Log
+
+
+def sentry_before_send_log(log: Log, _hint: Hint) -> Log | None:
+    """Filter out log messages that we don't want to send to Sentry Logs."""
+
+    if log.get("attributes", {}).get("logger.name") == "gunicorn.access":
+        return None
+
+    return log
+
 
 SENTRY_FILTERS = []
