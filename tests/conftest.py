82b2774bf63de41ab1c4d7951794b7340c5ad472
Santiago Regusci <santicomp2@gmail.com>
2026-01-29 14:36:22 -0300

    test supadata

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,6 +32,7 @@ def pyramid_settings():
         "youtube_transcripts": True,
         "api_jwt_secret": "secret",
         "youtube_api_key": "test_youtube_api_key",
+        "supadata_api_key": "test_supadata_api_key",
         "database_url": environ["DATABASE_URL"],
     }
 
